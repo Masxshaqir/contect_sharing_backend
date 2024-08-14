e0184d8c1f848140f4158bcbcbe0517a35e4bfb6
Masxshaqir <chkir44@gmail.com>
2024-08-14 22:33:06 +0300

    collect static

--- contect_sharing/settings.py
+++ contect_sharing/settings.py
@@ -47,8 +47,10 @@ INSTALLED_APPS = [
     'post'
 ]
 
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
